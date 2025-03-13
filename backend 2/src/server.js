76654c0b19048013ccc90677102333afeb702d6d
chieftitan88 <chief.store88@gmail.com>
2025-03-13 01:21:35 -0400

    Add files via upload

--- backend 2/src/server.js
+++ backend 2/src/server.js
@@ -91,7 +91,7 @@ app.use((err, req, res, next) => {
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  //app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
